c0bec17741927e386cb7f344354330dc17b3a349
Stephy <507452965@qq.com>
2019-02-24 18:04:20 +0800

    Route Request Param Test

--- application/sample/controller/Test.php
+++ application/sample/controller/Test.php
@@ -8,13 +8,34 @@
 
 namespace app\sample\controller;
 
-
+use think\facade\Request;
 class Test
 {
+
     //http://localhost:8085/Rebel/public/index.php/sample/test/hello
-    //通过虚拟路由简化项目URL路径
+    //通过虚拟路由简化项目URL路径-->z.cn:8085/sample/test/hello
 //F:\Tools\xampp\apache\conf\extra创建虚拟主机z.cn
-    public function hello(){
+    public function hello($id,$name){
+        echo $id;
+        echo '|';
+        echo $name;
         return "hello,Stegnography!";
+    }
+
+    public function request(){
+/*      $id =Request::param('id');
+        $name = Request::param('name');
+        echo $id;
+        echo '|';
+        echo $name;*/
+    /*    echo Request::param('id');
+        echo Request::param('name');*/
+//        echo $request->id;
+//        echo $name->name;
+        echo Request::param('id');
+        echo Request::param('name');
+        echo Request::param('age');
+/*        $all = input('param.');
+        var_dump($all);*/
     }
 }
--- route/route.php
+++ route/route.php
@@ -9,12 +9,22 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-Route::get('think', function () {
+/*Route::get('think', function () {
     return 'hello,ThinkPHP5!';
 });
 
 Route::get('hello/:name', 'index/hello');
 
+Route::rule('hello','sample/Test/hello');
 return [
 
-];
+];*/
+//use think\Route;
+/* 路由规则： Route::rule('路由表达式','路由地址','请求方式','路由参数（数组）','变量规则(数组）');
+ *  'url_lazy_route'         => false,开启强制使用路由
+ */
+//Route::rule('hello','sample/Test/hello');
+//Route::rule('hello','sample/Test/hello','GET|POST',['https'=>false]);
+Route::get('hello/:id','sample/Test/hello');
+Route::post('request/:id','sample/Test/request');
+//Route::any();
